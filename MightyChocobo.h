1301a2c8ca31084d903557aa1f9944efdcc4c5d6
CascoEresTu <k_k_ryuten@live.com>
2016-12-11 18:59:54 -0600

    hola

---
+++ MightyChocobo.h
@@ -0,0 +1,19 @@
+#include "Boss.h"
+#include "Person.h"
+#include <vector>
+
+class MightyChocobo : public Boss{
+private:
+string cry;
+
+public:
+  MightyChocobo();
+  MightyChocobo(string nombre,int hp, string warcry);
+  ~MightyChocobo();
+  void attack(vector<Person*>);
+
+
+
+
+
+}
